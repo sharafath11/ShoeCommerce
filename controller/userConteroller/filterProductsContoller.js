d5dca90eea92a882999e5bc31cb3b8b9950dc4e3
sharfath11 <121448400+sharfath11@users.noreply.github.com>
2024-10-09 11:14:27 +0530

    product filter

--- controller/userConteroller/filterProductsContoller.js
+++ controller/userConteroller/filterProductsContoller.js
@@ -3,46 +3,23 @@ import { categoryModel } from "../../models/category.js";
 import ProductModel from "../../models/prodectsModel.js";
 import Wishlist from "../../models/whislistModel.js";
 
-
 export const filteredProducts = async (req, res) => {
   try {
-    const user=req.session.user
-    const { brand, color } = req.body;
     let query = {};
-    if (brand && typeof brand === 'string' && brand.trim() !== "") {
-      query.brand = brand.trim();
+    if (req.body.brand) {
+      query.brand = req.body.brand;
     }
-    if (color && typeof color === 'string' && color.trim() !== "") {
-      query.color = color.trim();
-    }
-    query.block = false;
-    const products = await ProductModel.find(query);
-    const wishlist = await Wishlist.findOne({ user: user._id });
-    const wishlistProducts = wishlist && wishlist.products ? wishlist.products : [];
-    const uniqueProducts = [...new Set(wishlistProducts)];
-    const WishlistQty = uniqueProducts.length;
-    const categories = await categoryModel.find();
-    const cartItem = await CartModel.findOne({ userId: user._id }).populate("products.productId");
-    const cartQty = cartItem?.products?.length || 0;
 
-    if (!user || !user._id) {
-        return res.render("user/shopeDetials", {
-          user: user,
-          products: products || [],
-          WishlistQty: 0,
-          categories,
-          cartQty: req.session.cartQty,
-        });
-      }
-    return res.render("user/shopeDetials", {
-      user,
-      WishlistQty,
-      cartQty,
-      categories,
-      products,
-    });
+    if (req.body.color) {
+      query.color = req.body.color;
+    }
+    query.blocked = false; 
+    const products = await ProductModel.find(query);
+
+    
+   return res.json(products)
   } catch (error) {
-    console.error("Error fetching filtered products:", error);
-    res.status(500).json({ error: "An error occurred while fetching products." });
+    console.error('Error fetching products:', error);
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
