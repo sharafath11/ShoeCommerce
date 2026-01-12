6dbd0559e34a50957d56511faef8ecd89458a468
sharafath11 <abisharfath@gmail.com>
2026-01-12 21:22:35 +0530

    admin login bug  i checkiong to trust proxy

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 import express from "express";
 import userRoutes from "./routes/userRoutes.js";
-import adminRoutes from "./routes/adminRoutes.js"
+import adminRoutes from "./routes/adminRoutes.js";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import passport from "passport";
 import mongoose from "mongoose";
-import session from 'express-session'
-import MongoStore from 'connect-mongo';
-
-
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
+app.set("trust proxy", 1);
+
 app.use(passport.initialize());
+
 app.use(
   session({
     secret: process.env.SECRET,
@@ -32,32 +36,33 @@ app.use(
   })
 );
 
-
 mongoose
   .connect(process.env.MONGO_URL)
-  .then((res) => {
-    console.log(`databse connected succes full`);
+  .then(() => {
+    console.log("database connected successfully");
   })
   .catch((error) => {
     console.log(error);
   });
 
-
-
 app.set("view engine", "ejs");
 app.set("views", "./Views");
+
 app.use(bodyParser.json());
-app.use(express.static('public'))
-app.use(express.static('public')); 
-app.use(express.static("views/user"));
-app.use(express.static("views/admin"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+app.use(express.static("views/user"));
+app.use(express.static("views/admin"));
+
 app.use("/", userRoutes);
 app.use("/admin", adminRoutes);
-app.use((req, res, next) => {
-  res.status(404).render('user/404');
+
+app.use((req, res) => {
+  res.status(404).render("user/404");
 });
+
 app.listen(PORT, () => {
- console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
